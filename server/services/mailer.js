4a86cfffa8b81d05a6d8da0ff69ba0c9308a2f15
vguillemette <vguillemette@devalto.com>
2018-05-04 16:14:31 -0400

    smtp and port

--- server/services/mailer.js
+++ server/services/mailer.js
@@ -1,9 +1,9 @@
 'use strict';
 let nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
-	host: 'smtp.ethereal.email',
-	port: 587,
-	secure: false, // true for 465, false for other ports
+	host: process.env.NODE_ENV === 'prod' ? 'smtp.gmail.com ' : 'smtp.ethereal.email',
+	port: process.env.NODE_ENV === 'prod' ? 465 : 587,
+	secure: process.env.NODE_ENV === 'prod', // true for 465, false for other ports
 	auth: {
 		user: process.env.NODE_ENV === 'prod' ? 'admin@penpalms.com' : 'xqshdvio7ytxihl4@ethereal.email',
 		pass: process.env.NODE_ENV === 'prod' ? 'Rudolphe1' : 'VavhfQdxWeCbN6HD2n'
@@ -18,7 +18,7 @@ let transporter = nodemailer.createTransport({
 // 	html: '<b>Hello world?</b>'
 // };
 
-module.exports.sendMail = function (options) {
+module.exports.sendMail = function(options) {
 	transporter.sendMail(options, (error, info) => {
 		if (error) {
 			return console.log(error);
